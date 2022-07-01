85a25d1ba9f616ae04172362143a2f2f074d81ab
= <=>
2022-07-01 02:49:02 +0100

    Import a simp;e function from a simple file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import add_0 as add_lib 
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add_lib.add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first interger
+        b: second interger
+
+    Returns:
+        The return value, a + b
+    """
+    return (a + b)
