57c4e4bb42d524948c5fc99ff5116ad6cad4cb17
= <=>
2022-07-04 01:55:20 +0100

    ehance code to resolve fake values, pycodestyle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import calculator_1 as calc
     a = 10
     b = 5
-    print("10 + 5 = {}".format(calc.add(a,b)))
-    print("10 - 5 = {}".format(calc.sub(a,b)))
-    print("10 * 5 = {}".format(calc.mul(a,b)))
-    print("10 / 5 = {}".format(calc.div(a,b)))
+    print("10 + 5 = {:d}".format(calc.add(a, b)))
+    print("10 - 5 = {:d}".format(calc.sub(a, b)))
+    print("10 * 5 = {:d}".format(calc.mul(a, b)))
+    print("10 / 5 = {:d}".format(calc.div(a, b)))
