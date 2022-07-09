0e77f354a98ac329144b67f68ed92939d86d5728
Joseph Tam <tamj963@gmail.com>
2022-07-08 18:09:41 -0700

    File: 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-import add_0 as add_lib
-__name__ = __main__
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_lib.add(a, b)))
-=======
 if __name__ == "__main__":
-    import add_0 as add_lib
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_lib.add(a, b)))
->>>>>>> 57c4e4bb42d524948c5fc99ff5116ad6cad4cb17
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
