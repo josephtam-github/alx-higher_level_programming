17d722231b0402f514976cb7ba60cea7295e5603
josephtam-github <josephtam247@gmail.com>
2022-10-05 06:07:32 -0700

    feat: add 103-object_fct.js file

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
-  this.value += 1; 
-}
+myObject.incr = function () {
+  this.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
